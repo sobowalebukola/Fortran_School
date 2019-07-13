ab6aa84a2ee8894f1cf6cf424ec771a1055f7b8d
Bukola <sobowalebukola@gmail.com>
2019-07-13 16:06:44 +0100

    A fortran snippet that calculates the roots of a quadratic equation

--- new.f90
+++ new.f90
@@ -8,19 +8,17 @@ implicit none
     print*, 'Enter the coefficient of x^2 , coefficient of x and the constant respectively separated by commas'
     read*, a,b,c
     zp = ((-b)+d)/(2*a)
-    print*, zp
     d = (b*b) - (4*a*c)
     if(d > 0) then
         zp = ((-b)+sqrt(d))/(2*a)
         zn = ((-b)-sqrt(d))/(2*a)
-    end if
-    if(d == 0) then
+        else if(d == 0) then
              zp = ((-b)+sqrt(d))/2*a
              zp = zn
              print*, 'This root is equal'
         else 
             print*, 'This root is a complex root'
     end if 
-    print*, 'The first root is ', zp ,'and the second root is ', zn
+    print*, 'The first root is', zp ,'and the second root is ', zn
     
 end program test
